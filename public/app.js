568b16e352f3d370aa0bba637ef7eef747352c4b
nacimebelarbi-stack <nacimebelarbi@gmail.com>
2026-03-05 13:27:28 +0400

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,23 +1,69 @@
+Cesium.Ion.defaultAccessToken = "YOUR_TOKEN_HERE";
 
-Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0M2ZlMDNlMS05YmQ3LTQ2MWUtYTA2NC1iZWY5N2IwNTc4NDQiLCJpZCI6Mzk4NDk4LCJpYXQiOjE3NzI3MDAxMzB9.dqusYhifXL6vnbTMlGlOI1nP7ycmZzP4fVEw8Ixa_Dc";
-const socket = io({
-  transports: ["websocket"]
-});
+const socket = io({ transports: ["websocket"] });
+
 const viewer = new Cesium.Viewer("cesiumContainer", {
   timeline:false,
   animation:false,
   baseLayerPicker:false
 });
 
-socket.on("satellites", sats=>{
-  viewer.entities.removeAll();
-  sats.forEach(s=>{
-    viewer.entities.add({
-      position: Cesium.Cartesian3.fromDegrees(s.lon,s.lat,s.altitude),
-      point:{pixelSize:5,color:Cesium.Color.YELLOW},
-      label:{text:s.name,font:"8px monospace"}
-    });
-  });
-
+viewer.camera.flyTo({
+  destination: Cesium.Cartesian3.fromDegrees(50, 25, 20000000)
 });
 
+const satEntities = {};
+const flightEntities = {};
+
+socket.on("satellites", sats => {
+
+  Object.values(satEntities).forEach(e => viewer.entities.remove(e));
+  Object.keys(satEntities).forEach(k => delete satEntities[k]);
+
+  sats.forEach(s => {
+
+    const entity = viewer.entities.add({
+      position: Cesium.Cartesian3.fromDegrees(
+        s.lon, s.lat, s.altitude
+      ),
+      point: { pixelSize:6, color:Cesium.Color.RED },
+      label: {
+        text: s.name,
+        font: "10px monospace",
+        fillColor: Cesium.Color.YELLOW
+      },
+      polyline: {
+        positions: s.orbit.map(p =>
+          Cesium.Cartesian3.fromDegrees(p[0], p[1], p[2])
+        ),
+        width:1,
+        material: Cesium.Color.ORANGE
+      }
+    });
+
+    satEntities[s.name] = entity;
+  });
+});
+
+socket.on("flights", flights => {
+
+  Object.values(flightEntities).forEach(e => viewer.entities.remove(e));
+  Object.keys(flightEntities).forEach(k => delete flightEntities[k]);
+
+  flights.forEach(f => {
+
+    const entity = viewer.entities.add({
+      position: Cesium.Cartesian3.fromDegrees(
+        f.lon, f.lat, f.altitude
+      ),
+      point: { pixelSize:5, color:Cesium.Color.CYAN },
+      label: {
+        text: f.callsign || "FLIGHT",
+        font: "10px monospace",
+        fillColor: Cesium.Color.CYAN
+      }
+    });
+
+    flightEntities[f.callsign] = entity;
+  });
+});
